35b2ff3da2561d5bf358d3428d4c3c0786a86a8c
HectorSaldes <20193tn070@utez.edu.mx>
2022-09-03 22:00:19 -0500

    dev: Add sections of landing

---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,10 @@
+export default function Acerca() {
+  return (
+    <div
+    className='container my-14'>
+    <h2 className='mb-5 text-2xl md:text-4xl text-center font-bold text-primary'>
+      Contacto
+    </h2>
+  </div>
+  )
+}
---
+++ src/pages/Testimonios.jsx
@@ -0,0 +1,9 @@
+export default function Testimonios() {
+	return (
+		<div className='container my-14'>
+			<h2 className='mb-5 text-2xl md:text-4xl text-center font-bold text-primary'>
+				Testimonios
+			</h2>
+		</div>
+	);
+}
